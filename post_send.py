8b912abafbd69182c9f585ca25416a68ee79122a
Ershov Roman <romathebestprogramist@gmail.com>
2020-07-07 07:46:51 +0300

    Update post_send.py

--- post_send.py
+++ post_send.py
@@ -40,46 +40,66 @@ new_item.add_command(label='Редактирование')
 class Programm():
 	def __init__(self,root):
 
-		self.L0 = tk.Label(text = "Выберите файл который хотите отправить на серврер", font = "Arial 10", justify = tk.CENTER)
-		self.L1 = tk.Label(text = ":", font = "Arial 25", justify = tk.CENTER)
+		self.L0 = tk.Label(text = "Выберите файл который хотите отправить на серврер", font = "Arial 15", justify = tk.CENTER)
+		self.L1 = tk.Label(text = "Обязательно проверте запрос GET или POST", font = "Arial 10", justify = tk.CENTER)
+		self.L2 = tk.Label(text = ":", font = "Arial 25", justify = tk.CENTER)
 		
-		self.dirs = tk.Entry(width = 25)
-		self.url = tk.Entry(width = 25)
-		self.data = tk.Entry(width = 12)
-		self.data_value = tk.Entry(width = 12)
+		self.dirs = tk.Entry(width = 40)
+		self.url = tk.Entry(width = 40)
+		self.data = tk.Entry(width = 20)
+		self.data_value = tk.Entry(width = 20)
 
-		self.button_file = tk.Button(root, text = "File", command =self.file_dir)
-		self.button_data = tk.Button(root, text = "Add", command =self.data_send)
-		self.button_delete = tk.Button(root, text = "Delete", command =self.data_delete)
-		self.button_edit = tk.Button(root, text = "Edit", command =self.data_edit)
-		self.send_file = tk.Button(root, text = "Send", command =self.send)
-		self.button_test = tk.Button(root, text = "Test", command =self.test)
+		self.button_files_dir = tk.Button(root, text = "...", command =self.file_dir)
+		self.button_data_add = tk.Button(root, text = "Добавить", command =self.data_add)
+		self.button_data_edit = tk.Button(root, text = "Изменить", command =self.data_edit)
+		self.button_data_delete = tk.Button(root, text = "Удалить", command =self.data_delete)
+		self.button_send_data_POST = tk.Button(root, text = "Отправить POST", command =self.send_POST)
+		self.button_send_data_GET = tk.Button(root, text = "Отправить GET", command =self.send_GET)
+
+		self.button_test = tk.Button(root, text = "ТестКнопка", command =self.test)
 		
 		self.list_box_2 = tk.Listbox()
 
 		self.L0.grid(row = 0, column = 2)
-		self.L1.grid(row = 1, column = 2)
+		self.L1.grid(row = 3, column = 2, rowspan = 1)
+		self.L2.grid(row = 1, column = 2)
 		
-		self.url.grid(row = 1, column = 1)
+		self.url.grid(row = 1, column = 1, padx = 20)
 		self.data.grid(row = 1, column = 1, columnspan = 2)
 		self.data_value.grid(row = 1, column = 2, columnspan = 2)
-		self.dirs.grid(row = 1, column = 3)
+		self.dirs.grid(row = 1, column = 3, pady = 10, padx = 20)
 
-		self.send_file.grid(row = 2, column = 1)
+		self.button_send_data_POST.grid(row = 2, column = 1)
+		self.button_send_data_GET.grid(row = 3, column = 1)
 
-		self.button_edit.grid(row = 2, column = 1, columnspan = 2)
-		self.button_data.grid(row = 2, column = 2, columnspan = 1)
-		self.button_delete.grid(row = 2, column = 2, columnspan = 3)
-		self.button_file.grid(row = 2, column = 3)
+		self.button_data_edit.grid(row = 2, column = 1, columnspan = 2)
+		self.button_data_add.grid(row = 2, column = 2, columnspan = 1)
+		self.button_data_delete.grid(row = 2, column = 2, columnspan = 3)
+		self.button_files_dir.grid(row = 2, column = 3)
 		
 		self.button_test.grid(row = 3, column = 3)
 
 		self.list_box_2.grid(row = 5 ,column = 2, sticky = tk.W+tk.E)
 
-		self.dirs.insert(0,'Введите данные которые хотите отправить на сервер')
+		self.dirs.insert(0,'Выберите файл')
 		self.url.insert(0,'http://httpbin.org/post')
 		
 		self.list_box_2.bind("<<ListboxSelect>>", self.test)
+
+	def file_dir(self):
+		file = filedialog.askopenfilename()
+		
+		self.dirs.insert(0, f"{file},")
+
+	def data_add(self):
+		data = self.data.get()
+		data_value = self.data_value.get()
+
+		if data == '':
+			pass
+		else:
+			sel = self.list_box_2.curselection()
+			self.list_box_2.insert(0, f'{data} : {data_value}')
 
 	def data_edit(self):
 		data = self.data.get()
@@ -90,36 +110,6 @@ class Programm():
 		self.list_box_2.delete(index, index)
 		self.list_box_2.insert(0, f'{data} : {data_value}')
 
-
-	def test(self,get):
-		data = self.list_box_2.get(self.list_box_2.curselection())
-		
-		data_key = data.split(" : ")[0]
-		data_value = data.split(" : ")[1]
-
-		self.data.delete(0, tk.END)
-		self.data_value.delete(0, tk.END)
-
-		self.data.insert(0, data_key)
-		self.data_value.insert(0, data_value)
-
-
-	def file_dir(self):
-		file = filedialog.askopenfilename()
-		
-		self.dirs.delete(0, tk.END)
-		self.dirs.insert(0, f"{file},")
-
-	def data_send(self):
-		data = self.data.get()
-		data_value = self.data_value.get()
-
-		if data == '':
-			pass
-		else:
-			sel = self.list_box_2.curselection()
-			self.list_box_2.insert(0, f'{data} : {data_value}')
-
 	def data_delete(self):
 		sel = self.list_box_2.curselection()
 
@@ -128,11 +118,17 @@ class Programm():
 		else:
 			self.list_box_2.delete(sel[0])
 
-	def send(self):
+	def send_POST(self):
 		dirs = self.dirs.get()
 		url = self.url.get()
 		dir_file = dirs.split(',')
-		files = {'file' : dir_file}
+		dir_file.pop()
+		if dir_file == []:
+			files = ""
+		else:
+			for file in dir_file:
+				files = {'file' : file}
+
 		data_box = {}
 
 		for data in self.list_box_2.get(0, tk.END):
@@ -143,21 +139,87 @@ class Programm():
 			else:
 				data_box[data_key] = data_value
 
-		s = requests.post(url, data = data_box, files= files if files == "" else None)
+		s = requests.post(url, json = data_box, files= files if files == "" else None)
 		log = s.text
-		json_data = json.loads(log)
+
+		try:
+			json_data = json.loads(log)
+		except json.decoder.JSONDecodeError:
+			self.L1['text'] = "Ошибка POST JSON данные не получены"
+			return 
+
 		list_box = tk.Listbox()
+		
 		list_box.grid(row = 4 ,column = 2, sticky = tk.W+tk.E)
 
 		for name in json_data:
 			if name == "headers":
 				for name_head in json_data['headers']:
-					list_box.insert(0, f"{name_head} : {json_data['headers'][name_head]}")
+					list_box.insert(0, f"    {name_head} --> {json_data['headers'][name_head]}")
+				list_box.insert(0, f"{name} -->")
 			else:
 				if json_data[name] == None or json_data[name] == {} or json_data[name] == " ":
 					pass
 				else:
-					list_box.insert(0, f"{name} : {json_data[name]}")
+					list_box.insert(0, f"{name} --> {json_data[name]}")
+
+	def send_GET(self):
+		dirs = self.dirs.get()
+		dir_file = dirs.split(',')
+		dir_file.pop()
+
+		url = self.url.get()
+		data = self.list_box_2.get(0, tk.END)
+		param = {}
+
+		if dir_file == []:
+			files = ""
+		else:
+			for file in dir_file:
+				files = {'file' : file}
+
+		for str_data in data:
+			data_key = str_data.split(" : ")[0]
+			data_value = str_data.split(" : ")[1]
+
+			param[data_key] = data_value
+
+		s = requests.get(url, params = param, files = files)
+		log = s.text
+
+		try:
+			json_data = json.loads(log)
+		except json.decoder.JSONDecodeError:
+			self.L1['text'] = "Ошибка GET JSON данные не получены"
+			return 
+
+		list_box = tk.Listbox()
+		list_box.grid(row = 4 ,column = 2, sticky = tk.W+tk.E)
+
+		for key in json_data:
+			if key == "headers":
+				for headers_data in json_data[key]:
+					list_box.insert(0, f"    {headers_data} --> {json_data[key][headers_data]}")
+				list_box.insert(0, f"{key} --> ")
+			else:
+				list_box.insert(0, f"{key} --> {json_data[key]}")
+
+	def test(self):
+		dirs = self.dirs.get()
+		dir_file = dirs.split(',')
+		dir_file.pop()
+		print(dir_file)
+		return
+		data = self.list_box_2.get(self.list_box_2.curselection())
+		
+		data_key = data.split(" : ")[0]
+		data_value = data.split(" : ")[1]
+
+		self.data.delete(0, tk.END)
+		self.data_value.delete(0, tk.END)
+
+		self.data.insert(0, data_key)
+		self.data_value.insert(0, data_value)
 s = Programm(root)
 
 root.mainloop()
